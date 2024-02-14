d668a01fde66c33c631f5acb1485250d4d2b0950
Jon-Felipe <rodriguesjf7@gmail.com>
2024-02-14 09:31:24 +0000

    navbar content and styling

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,7 +1,55 @@
+import styled from 'styled-components';
+
 type Props = {};
 
 function Navbar({}: Props) {
-  return <div>Navbar</div>;
+  return (
+    <Wrapper>
+      <h2>Jobli</h2>
+      <ul className='nav-links'>
+        <li>
+          <a href='#'>Home</a>
+        </li>
+        <li>
+          <a href='#'>About</a>
+        </li>
+        <li>
+          <a href='#'>Job</a>
+        </li>
+        <li>
+          <a href='#'>Contact</a>
+        </li>
+      </ul>
+      <button type='button' className='btn'>
+        Join Now
+      </button>
+    </Wrapper>
+  );
 }
 
 export default Navbar;
+
+const Wrapper = styled.nav`
+  display: grid;
+  grid-template-columns: 1fr auto;
+  align-items: center;
+  height: 5rem;
+  max-width: 1320px;
+  margin: 0 auto;
+  padding: 0 2rem;
+  .nav-links {
+    display: none;
+  }
+  @media (min-width: 1024px) {
+    grid-template-columns: 1fr 2fr auto;
+    .nav-links {
+      display: flex;
+      align-items: center;
+      column-gap: 20px;
+      a {
+        font-size: 1.25rem;
+        font-weight: 500;
+      }
+    }
+  }
+`;
