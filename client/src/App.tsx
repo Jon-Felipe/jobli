3eb7abbeaa795020a2da5e03f1741e7661a19b6a
Jon-Felipe <rodriguesjf7@gmail.com>
2024-02-14 16:00:46 +0000

    created home page initial

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,11 +2,18 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 // pages
 import MainLayout from './pages/MainLayout';
+import Home from './pages/Home';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <MainLayout />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+    ],
   },
 ]);
 
