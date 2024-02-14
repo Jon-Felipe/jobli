dc3f59ec185eb3e7a19e1c7ef1528074e916f478
Jon-Felipe <rodriguesjf7@gmail.com>
2024-02-14 08:32:23 +0000

    setup react router provider

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,14 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <div>Hello World</div>,
+  },
+]);
+
 function App() {
-  return (
-    <main>
-      <h1>Hello World</h1>
-    </main>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
