5e8199c8e49c0c232679eff9c68f3220f78a8c59
Jon-Felipe <rodriguesjf7@gmail.com>
2024-02-16 08:37:51 +0000

    job category type and constants

---
+++ client/src/utils/constants.ts
@@ -0,0 +1,40 @@
+import { JobCategory } from './types';
+
+export const jobCategory: JobCategory[] = [
+  {
+    id: 1,
+    icon: '',
+    title: 'Design & Creatives',
+    text: 'The automated process starts as soon as your clothes go into',
+  },
+  {
+    id: 2,
+    icon: '',
+    title: 'Finance',
+    text: 'The automated process starts as soon as your clothes go into',
+  },
+  {
+    id: 3,
+    icon: '',
+    title: 'Marketing',
+    text: 'The automated process starts as soon as your clothes go into',
+  },
+  {
+    id: 4,
+    icon: '',
+    title: 'Health/Medical',
+    text: 'The automated process starts as soon as your clothes go into',
+  },
+  {
+    id: 5,
+    icon: '',
+    title: 'Corporate',
+    text: 'The automated process starts as soon as your clothes go into',
+  },
+  {
+    id: 6,
+    icon: '',
+    title: 'Copywriting',
+    text: 'The automated process starts as soon as your clothes go into',
+  },
+];
---
+++ client/src/utils/types.ts
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export type JobCategory = {
+  id: number;
+  icon: React.ReactNode;
+  title: string;
+  text: string;
+};
