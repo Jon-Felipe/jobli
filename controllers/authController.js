cfdd2d1d135fb46a191daf140a12894558ae680d
Jon-Felipe <rodriguesjf7@gmail.com>
2024-02-24 18:11:08 +0000

    register route initial

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,5 @@
 // @desc    Register user
-// @route   Post /api/users/register
+// @route   Post /api/v1/auth/register
 // @access  Public
 export const register = async (req, res) => {
   res.send('register');
--- server.js
+++ server.js
@@ -3,12 +3,17 @@ dotenv.config();
 import express from 'express';
 const app = express();
 
+// routers
+import authRoutes from './routes/authRoutes.js';
+
 // middleware
 app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
+
+app.use('/api/v1/auth', authRoutes);
 
 const PORT = process.env.PORT || 5100;
 
