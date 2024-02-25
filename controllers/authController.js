e9e6ed4758cc06cb4764a75e899c57276f31fa0e
Jon-Felipe <rodriguesjf7@gmail.com>
2024-02-25 09:04:35 +0000

    register user controller functionality

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,8 +1,12 @@
+// extras
+import User from '../models/UserModel';
+
 // @desc    Register user
 // @route   Post /api/v1/auth/register
 // @access  Public
 export const register = async (req, res) => {
-  res.send('register');
+  await User.create(req.body);
+  res.status(200).json({ msg: 'user created' });
 };
 
 // @desc    Login user
