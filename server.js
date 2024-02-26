f5ace5c53d64a48cade3b3a69cf5d711a2a4b042
Jon-Felipe <rodriguesjf7@gmail.com>
2024-02-26 09:55:38 +0000

    mongodb connection

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ dotenv.config();
 import express from 'express';
 const app = express();
 import { StatusCodes } from 'http-status-codes';
+import mongoose from 'mongoose';
 
 // routers
 import authRoutes from './routes/authRoutes.js';
@@ -27,4 +28,10 @@ app.use(errorHandlerMiddleware);
 
 const PORT = process.env.PORT || 5100;
 
-app.listen(PORT, () => console.log(`server running on port ${PORT}...`));
+try {
+  await mongoose.connect(process.env.MONGO_URL);
+  app.listen(PORT, () => console.log(`server running on port ${PORT}...`));
+} catch (error) {
+  console.log(error);
+  process.exit(1);
+}
