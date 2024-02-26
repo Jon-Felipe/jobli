9b94a8a93182dfb8498bcfaf2f8f95f757a8a978
Jon-Felipe <rodriguesjf7@gmail.com>
2024-02-26 10:10:20 +0000

    hashing user password

--- models/UserModel.js
+++ models/UserModel.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
 
 const UserSchema = new mongoose.Schema({
   email: {
@@ -19,6 +20,11 @@ const UserSchema = new mongoose.Schema({
     required: true,
     min: [6, 'Password should be at least 6 characters in length.'],
   },
+});
+
+UserSchema.pre('save', async function () {
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
 });
 
 export default mongoose.model('User', UserSchema);
