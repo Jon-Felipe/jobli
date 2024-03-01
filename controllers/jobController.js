0932fcc5e62176c2d9451b7ec4891dcd5c967890
Jon-Felipe <rodriguesjf7@gmail.com>
2024-03-01 19:31:39 +0000

    git single job functionality

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -1,6 +1,7 @@
 import { StatusCodes } from 'http-status-codes';
 
 import Job from '../models/JobModel.js';
+import { NotFoundError } from '../errors/customErrors.js';
 
 // @desc    Get all jobs
 // @route   GET /api/v1/jobs
@@ -16,5 +17,10 @@ export const getAllJobs = async (req, res) => {
 // @route   GET /api/v1/jobs/:id
 // @access  Public
 export const getSingleJob = async (req, res) => {
-  res.send('get single job');
+  const job = await Job.findById(req.params.id);
+  if (!job) {
+    throw new NotFoundError(`No job found with id ${req.params.id}`);
+  }
+
+  res.status(StatusCodes.OK).json({ job });
 };
