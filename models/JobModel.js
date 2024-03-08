5864aef75241ae3ce0e656f0dd6360301964e0e6
Jon-Felipe <rodriguesjf7@gmail.com>
2024-03-08 10:03:39 +0000

    job model update

--- models/JobModel.js
+++ models/JobModel.js
@@ -1,16 +1,21 @@
 import mongoose from 'mongoose';
 
-const JobSchema = new mongoose.Schema({
-  jobTitle: { type: String, required: true },
-  jobDescription: { type: String },
-  companyName: { type: String, required: true },
-  location: { type: String },
-  salary: { type: Number },
-  jobNature: {
-    type: String,
-    enum: ['full-time', 'part-time', 'internship'],
-    default: 'full-time',
+const JobSchema = new mongoose.Schema(
+  {
+    jobTitle: { type: String, required: true },
+    jobDescription: { type: String },
+    requiredSkills: { type: [String] },
+    experience: { type: [String] },
+    location: { type: String },
+    salary: { type: Number },
+    jobNature: {
+      type: String,
+      enum: ['full-time', 'part-time', 'internship'],
+      default: 'full-time',
+    },
+    companyName: { type: String, required: true },
   },
-});
+  { timestamps: true }
+);
 
 export default mongoose.model('Job', JobSchema);
