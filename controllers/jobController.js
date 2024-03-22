9b10369be0ebcc5cd1ec3dc4de31180bfb2d7812
Jon-Felipe <rodriguesjf7@gmail.com>
2024-03-22 17:10:21 +0000

    get all jobs sort functionality

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -7,9 +7,20 @@ import { NotFoundError } from '../errors/customErrors.js';
 // @route   GET /api/v1/jobs
 // @access  Public
 export const getAllJobs = async (req, res) => {
+  const { sort } = req.query;
+
+  const sortOptions = {
+    newest: '-createdAt',
+    oldest: 'createdAt',
+    'a-z': 'jobTitle',
+    'z-a': '-jobTitle',
+  };
+
+  const sortKey = sortOptions[sort] || sortOptions.newest;
+
   const limit = Number(req.query.limit) || 10;
 
-  const jobs = await Job.find({}).limit(limit);
+  const jobs = await Job.find({}).sort(sortKey).limit(limit);
   const totalJobs = await Job.countDocuments();
 
   res.status(StatusCodes.OK).json({ totalJobs, jobs });
